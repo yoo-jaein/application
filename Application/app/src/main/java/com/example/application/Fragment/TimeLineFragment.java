a24a6a0101e3d794afed6d1ca088866f2a9c66de
yoo-jaein <y_jaein@naver.com>
2017-09-30 22:49:29 +0900

    틀만들기
    액티비티랑 프레그만듦

---
+++ Application/app/src/main/java/com/example/application/Fragment/TimeLineFragment.java
@@ -0,0 +1,14 @@
+package com.example.application.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.application.R;
+public class TimeLineFragment extends Fragment {
+ 
+}
