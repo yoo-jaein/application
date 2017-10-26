2283f777e6a00b87929358aa0363422cf2146dfc
yoo-jaein <y_jaein@naver.com>
2017-10-26 23:17:59 +0900

    로그인 바꾸는중. 재민이꺼랑 합침

--- MyApplication/app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
@@ -92,7 +92,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
 
         mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+
     }
 
     private void populateAutoComplete() {
