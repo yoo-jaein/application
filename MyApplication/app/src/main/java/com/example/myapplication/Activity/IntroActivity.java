2492273d7cbeae1e445b5c1df0dfdd48fd57046b
SongYeongJu <thddudwn1012@naver.com>
2017-10-30 04:21:01 +0900

    인트로 페이지 스킵버튼이랑 밑에 화면 넘기면 파란점 위치 이동구현

--- MyApplication/app/src/main/java/com/example/myapplication/Activity/IntroActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity/IntroActivity.java
@@ -6,6 +6,10 @@ import android.os.Handler;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.myapplication.Fragment.Help1Fragment;
 import com.example.myapplication.Fragment.Help2Fragment;
@@ -17,18 +21,24 @@ public class IntroActivity extends AppCompatActivity {
 
     ClientController client;
 
-    private Handler handler;
-
+    private Handler handlers;
 
     ViewPager vp;
+
+    ImageView select1;
+    ImageView select2;
+    ImageView select3;
 
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            try {
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }catch (Exception e) {
+            }
         }
     };
 
@@ -36,6 +46,8 @@ public class IntroActivity extends AppCompatActivity {
         @Override
         public void run() {
             vp.setCurrentItem(1);
+            select1.setImageResource(R.drawable.guideunselect);
+            select2.setImageResource(R.drawable.guideselect);
         }
     };
 
@@ -43,6 +55,8 @@ public class IntroActivity extends AppCompatActivity {
         @Override
         public void run() {
             vp.setCurrentItem(2);
+            select2.setImageResource(R.drawable.guideunselect);
+            select3.setImageResource(R.drawable.guideselect);
         }
     };
 
@@ -56,9 +70,27 @@ public class IntroActivity extends AppCompatActivity {
         client = ClientController.getClientControl();
         client.client.start();
 
-        handler.postDelayed(runnable1,4000);
-        handler.postDelayed(runnable2,8000);
-        handler.postDelayed(runnable, 14000);
+        Button skip=(Button)findViewById(R.id.guideskipButton);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("test","IntroActivity : skip Onclick Listener");
+                handlers.removeMessages(0);
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        select1=(ImageView)findViewById(R.id.guideselect1);
+        select2=(ImageView)findViewById(R.id.guideselect2);
+        select3=(ImageView)findViewById(R.id.guideselect3);
+
+        select1.setImageResource(R.drawable.guideselect);
+
+        handlers.postDelayed(runnable1,4000);
+        handlers.postDelayed(runnable2,8000);
+        handlers.postDelayed(runnable, 14000);
 
         vp = (ViewPager)findViewById(R.id.vp);
         vp.setAdapter(new pagerAdapter(getSupportFragmentManager()));
@@ -69,13 +101,13 @@ public class IntroActivity extends AppCompatActivity {
 
 
     public void init() {
-        handler = new Handler();
+        handlers = new Handler();
     }
 
     @Override
     public void onBackPressed(){
         super.onBackPressed();
-        handler.removeCallbacks(runnable);
+        handlers.removeCallbacks(runnable);
     }
 
 }
--- MyApplication/app/src/main/java/com/example/myapplication/Fragment/Help2Fragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Fragment/Help2Fragment.java
@@ -2,6 +2,7 @@ package com.example.myapplication.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,8 @@ public class Help2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Log.d("test","Help Fragment 2 : OnCreateView");
+
         return inflater.inflate(R.layout.fragment_help2, container, false);
 
     }
