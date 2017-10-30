eaf87469e7e9b2f5fda5a3b7624c91c95fcef6c4
zzizzh <zzizzh@nate.com>
2017-10-30 21:34:00 +0900

    Merge branch 'master' of https://github.com/yoo-jaein/application
    # Conflicts:
    #	MyApplication/app/src/main/java/com/example/myapplication/Activity/MainActivity.java
    #	MyApplication/app/src/main/res/layout/activity_main.xml

--- MyApplication/app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -23,7 +23,6 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
